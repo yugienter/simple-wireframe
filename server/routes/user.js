d240640f8d25d89efd61d46efecae189cc3edf1d
yugienter <phu.nguyenluu@gmail.com>
2021-07-24 17:10:35 +0700

    Finish Route Modal Controller

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const controllers = require("../controllers");
 const { user } = controllers;
 const { changePassword } = user;
 
